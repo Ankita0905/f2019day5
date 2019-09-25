0041f4e449dab502a06622fb6d454a5c40d5e7b5
macstudent <ankita.jain12342@gmail.com>
2019-09-24 21:00:43 -0400

    similar word search

--- src/main/java/com/lambton/CollectionFrameworkExample.java
+++ src/main/java/com/lambton/CollectionFrameworkExample.java
@@ -6,7 +6,7 @@ public class CollectionFrameworkExample
 {
     public static void main(String[] args)
     {
-        ArrayList<String> mStringArrayList=new ArrayList<>();
+       /* ArrayList<String> mStringArrayList=new ArrayList<>();
 
         mStringArrayList.add("A");
         mStringArrayList.add("B");
@@ -19,7 +19,7 @@ public class CollectionFrameworkExample
        for(int i=0;i<mStringArrayList.size();i++)
        {
            System.out.println(mStringArrayList.get(i));
-       }
+       }*/
 
        /*for(String num: mStringArrayList)
        {
@@ -63,7 +63,58 @@ public class CollectionFrameworkExample
         System.out.println(stringMap);
 
 
+        //new cass exercise
 
+        /*ArrayList<String> data=new ArrayList<>();
+        data.add("abc");
+        data.add("cab");
+        data.add("cat");
+        data.add("mate");
+        data.add("atem");
+        data.add("bac");
+        data.add("tac");
+        data.add("atc");
+        data.add("pop");
+        data.add("pat");
+        data.add("123");
+        data.add("231");
+        data.add("431");
+
+        System.out.println(data);
+
+        System.out.println();*/
+        ArrayList<String>  mStringArrayList = new ArrayList<>();
+        mStringArrayList.add("abc");
+        mStringArrayList.add("cab");
+        mStringArrayList.add("cat");
+        mStringArrayList.add("mate");
+        mStringArrayList.add("atem");
+        mStringArrayList.add("bac");
+        mStringArrayList.add("tac");
+        mStringArrayList.add("atc");
+        mStringArrayList.add("pop");
+        mStringArrayList.add("pat");
+        mStringArrayList.add("123");
+        mStringArrayList.add("231");
+        mStringArrayList.add("431");
+        Map<String,ArrayList<String>> hm=new HashMap<>();
+
+        for(String s:mStringArrayList)
+        {
+            char[] c=s.toCharArray();
+            Arrays.sort(c);
+            String key=new String(c);
+
+            ArrayList<String>al=hm.get(key);
+            if(al==null)
+            {
+                al=new ArrayList<>();
+            }
+            al.add(s);
+            hm.put(key,al);
+
+        }
+        System.out.println(hm);
 
     }
 }
